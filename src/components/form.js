42a6c800b760895743ebfd840eff037c743e6568
Emre Mert <emre.mrt98@gmail.com>
2022-08-23 15:03:48 +0300

    v2

--- src/components/form.js
+++ src/components/form.js
@@ -1,18 +1,39 @@
 import { BiUserCircle } from "react-icons/bi";
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
-
+import { useEffect, useState } from "react";
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
+import toast, { Toaster } from 'react-hot-toast';
 export default function Form() {
+    const [eyeState, setEyeState] = useState(true);
+    const [eyeState2, setEyeState2] = useState(true);
+    const [value, setValue] = useState()
+
+    const control = () => { toast('Kayıt Başarılı', { style: { background: "green", color: "white" } }); }
+
     return (
         <div className="container">
             <BiUserCircle className="icon" />
             <h1>Sign In</h1>
-            <form className="form">
+            <form onSubmit={(e) => { e.preventDefault(); control(); }} className="form">
                 <input type="text" placeholder="Name" required />
                 <input type="email" placeholder="E-mail" required />
-                <input type="Number" placeholder="Phone Number" required /><AiFillEyeInvisible className="eye-icon" />
-                <input type="Password" placeholder="Password" required /><AiFillEyeInvisible className="eye-icon" />
-                <input type="Password" placeholder="Password Control" required />
-                <button>Sign In</button>
+
+                <PhoneInput style={{ color: "white" }} value={value} onChange={setValue} placeholder="Phone Number" required />
+                <input type={eyeState ? "Password" : "text"} placeholder="Password" required />
+                {eyeState ?
+                    <AiFillEyeInvisible onClick={() => setEyeState(!eyeState)} className="eye-icon eye1" />
+                    :
+                    <AiFillEye onClick={() => setEyeState(!eyeState)} className="eye-icon eye1" />
+                }
+                <input type={eyeState2 ? "Password" : "text"} placeholder="Password Control" required />
+                {eyeState2 ?
+                    <AiFillEyeInvisible onClick={() => setEyeState2(!eyeState2)} className="eye-icon eye2" />
+                    :
+                    <AiFillEye onClick={() => setEyeState2(!eyeState2)} className="eye-icon eye2" />
+                }
+                <button type="submit">Sign In</button>
+                <Toaster duration={5000} />
             </form>
         </div>
     )
